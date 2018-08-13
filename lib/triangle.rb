c492d0328b93d7a0bb5eb41a2c7692fbcdccd7be
Christopher Beltran <christod.beltran@gmail.com>
2018-08-13 04:33:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,14 @@ def initialize(side_one, side_two, side_three)
 end 
 
 def kind
-   if @side_one == @side_two && @side_one == @side_three
+  if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
+    raise TriangleError
+   elsif @side_one == @side_two && @side_one == @side_three
     return :equilateral
   elsif @side_one != @side_two && @side_one != @side_three && @side_two != @side_three
     return :scalene
   elsif @side_one == @side_two && @side_two != @side_three
     return :isosceles
-  elsif @side_one <= 0 || @side_two <= 0 || @side_three <= 0
-    raise TriangleError
     
     
 
